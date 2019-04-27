cf995d0de7ab14dc8250a3d6266346c084f6e898
Dominik Chodounsky <chododom@fit.cvut.cz>
2019-04-27 13:07:21 +0200

    Retrieved page ranks from ranking text file

--- hypertext_search_engine/src/config.py
+++ hypertext_search_engine/src/config.py
@@ -1,14 +1,14 @@
 HOMEPAGE = 'https://fit.cvut.cz'
-CRAWL = True
+CRAWL = False
 THREAD_CNT = 30
 PAGE_CNT = 1000
 CALC_PR = True and CRAWL        # CRAWL must be True in order for CALC_PR to work
 ALPHA = 0.85
-ITERATION_CNT = 50
+ITERATION_CNT = 100
 METHOD = "power"        # power / matrix
-INIT_SEARCH_INDEX = True
+INIT_SEARCH_INDEX = False
 SEARCH = True
-SEARCH_WORD = "Algoritmy"
+SEARCH_WORD = "Znalostní inženýrství"
 RESULT_CNT = 10
 
 PARENT_DIR = "../"
